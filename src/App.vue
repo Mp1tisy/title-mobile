cf06d6d47ee995b5d22fff2eb8f1e515a3ef2d64
yanpeiting <449108802@qq.com>
2020-01-08 15:17:05 +0800

    封装axios请求

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <van-icon name="chat-o"></van-icon>
-    <van-icon name="chat-o"></van-icon>
-    <van-icon name="chat-o"></van-icon>
-    <div class="test">test</div>
     <router-view></router-view>
   </div>
 </template>
@@ -20,9 +11,4 @@ export default {
 </script>
 
 <style>
-.test {
-  height: 200px;
-  width: 200px;
-  border: 1px solid red;
-}
 </style>
