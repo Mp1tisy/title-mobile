11c4e90e1fd4d8eab62cfa731d27f023d48193c8
yanpeiting <449108802@qq.com>
2020-01-08 15:36:00 +0800

    大数字处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,19 @@
 import axios from 'axios'
+import jsonbigint from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+
+request.defaults.transformRequest = [
+  function (data) {
+    try {
+      return jsonbigint.parse(data)
+    } catch (err) {
+      console.log('转换失败', err)
+      return {}
+    }
+  }
+]
 
 export default request
